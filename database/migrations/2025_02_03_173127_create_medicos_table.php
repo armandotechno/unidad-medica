afba470db34e10725c165629cb9ddf3d9631b652
Armando Capriles <armandotechno12@gmail.com>
2025-02-03 19:16:37 -0400

    Cambio en la migracion de medicos

--- database/migrations/2025_02_03_173127_create_medicos_table.php
+++ database/migrations/2025_02_03_173127_create_medicos_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('medicos', function (Blueprint $table) {
             $table->id()->comment('Identificador de la tabla');
             $table->string('nombre', 200)->nullable()->comment('Guarda el nombre del médico');
+            $table->integer('dni')->comment('Guarda el número de identificación del doctor');
+            $table->integer('cmp')->comment('Guarda el número de identificación del CMP');
+            $table->integer('doctor_created')->nullable()->comment("Guarda el id del usuario que crea al doctor");
+            $table->timestamp('created_at')->default(now())->comment("Guarda la fecha en que fue creado el doctor");
+            $table->integer('doctor_updated')->nullable()->comment("Guarda el id del usuario que actualiza al doctor");
+            $table->timestamp('updated_at')->nullable()->comment("Guarda la fecha en que fue actualizado el doctor");
         });
     }
 
